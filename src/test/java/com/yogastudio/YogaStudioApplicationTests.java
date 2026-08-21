ee6a4783bc75192ca2b9bc84d357f4c6d4e8bc2f
Iuliana Paun <veseletea@gmail.com>
2026-08-21 21:30:09 +0300

    test: add Testcontainers integration tests and Failsafe plugin

--- src/test/java/com/yogastudio/YogaStudioApplicationTests.java
+++ src/test/java/com/yogastudio/YogaStudioApplicationTests.java
@@ -1,10 +1,8 @@
 package com.yogastudio;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class YogaStudioApplicationTests {
+class YogaStudioApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
